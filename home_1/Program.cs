a4a538d3bebb9b86c8c3a1b693ea324f4b0c6ca6
Artyom Kychin <artem.kychin@mail.ru>
2023-03-31 19:26:59 +0300

    Добавьте файлы проекта.

---
+++ home_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string user = Console.ReadLine();//ввод имени пользователя
+            Console.WriteLine("Hello " + user +"!");
+            Console.WriteLine("Для закрытия окна нажмите любую клавишу");
+            Console.ReadKey();//ожмдание нажатия люой кнопки
+        }
+    }
+}
