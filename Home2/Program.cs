1e2870f45deb52df56f7dc06eefbb4473e8ba816
Artyom Kychin <artem.kychin@mail.ru>
2023-04-01 15:44:54 +0300

    calculator(if/else)

--- Home2/Program.cs
+++ Home2/Program.cs
@@ -6,17 +6,47 @@ namespace HomeApp
     {
         static void Main(string[] args)
         {
-            // Pi = 3.1415922653;
-            double pi = Math.PI;
-            //Console.WriteLine(Pi);
-            Console.WriteLine("input radius");
-            String radius = Console.ReadLine();
-            double radiusConverted = Convert.ToDouble(radius);
-            double result = pi * Math.Pow(radiusConverted, 2);
-            Console.WriteLine(result);
-            //double pishort = Math.Round (pi, 5);
-            //Console.WriteLine("Pi is: " + pi);
+            Console.WriteLine("Please, enter number 1");
+            double operand1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please, enter number 2");
+            double operand2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please, enter the desired arithmetic operation\nThe following actions are available: + , - , * , / ");
+            string sign = Convert.ToString(Console.ReadLine());
+
+            if (sign == "+")
+            {
+                double result = operand1 + operand2;
+
+                Console.WriteLine("The sum = " + result);
+            }
+            else if (sign == "-")
+            {
+                double result = operand1 - operand2;
+
+                Console.WriteLine("The difference = " + result);
+            }
+            else if (sign == "*")
+            {
+                double result = operand1 * operand2;
+                Console.WriteLine("The multiplication result = " + result);
+            }
+            else if (sign == "/")
+            {
+                if (operand2 == 0)
+                    Console.WriteLine("Action incorrect (/0)");
+                else
+                {
+                    double result = operand1 / operand2;
+                    Console.WriteLine("The quotient = " + result);
+                }
+            }
 
         }
+        
+       
+       
     }
+
 }
