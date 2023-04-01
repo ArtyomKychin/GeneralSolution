29b00af27c41df0284cca0f631dec2d15fc469d5
Artyom Kychin <artem.kychin@mail.ru>
2023-04-01 15:48:58 +0300

    calculator(switch)

--- Home2/Program.cs
+++ Home2/Program.cs
@@ -15,32 +15,43 @@ namespace HomeApp
             Console.WriteLine("Please, enter the desired arithmetic operation\nThe following actions are available: + , - , * , / ");
             string sign = Convert.ToString(Console.ReadLine());
 
-            if (sign == "+")
+            switch (sign)
             {
-                double result = operand1 + operand2;
+                case "+":
+                    {
+                        double result = operand1 + operand2;
 
-                Console.WriteLine("The sum = " + result);
-            }
-            else if (sign == "-")
-            {
-                double result = operand1 - operand2;
+                        Console.WriteLine("The sum = " + result);
+                        break;
+                    }
 
-                Console.WriteLine("The difference = " + result);
-            }
-            else if (sign == "*")
-            {
-                double result = operand1 * operand2;
-                Console.WriteLine("The multiplication result = " + result);
-            }
-            else if (sign == "/")
-            {
-                if (operand2 == 0)
-                    Console.WriteLine("Action incorrect (/0)");
-                else
-                {
-                    double result = operand1 / operand2;
-                    Console.WriteLine("The quotient = " + result);
-                }
+                case "-":
+                    {
+                        double result = operand1 - operand2;
+
+                        Console.WriteLine("The difference = " + result);
+                        break;
+                    }
+
+                case "*":
+                    {
+                        double result = operand1 * operand2;
+                        Console.WriteLine("The multiplication result = " + result);
+                        break;
+                    }
+
+                case "/":
+                    {
+                        if (operand2 == 0)
+                            Console.WriteLine("Action incorrect (/0)");
+                        else
+                        {
+                            double result = operand1 / operand2;
+                            Console.WriteLine("The quotient = " + result);
+                        }
+
+                        break;
+                    }
             }
 
         }
